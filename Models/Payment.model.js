f0bda66ef1824fef7c41c0ac68783e75b968ddc0
suganeswaran <nsuganeswaran@gmail.com>
2023-10-27 23:44:32 +0530

    Add payment model

---
+++ Models/Payment.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema(
+  {
+    bookingId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Booking",
+      required: true,
+    },
+    paymentAmount: {
+      type: Number,
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Payment = mongoose.model("Payment", paymentSchema);
+
+module.exports = Payment;
