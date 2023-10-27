c733df71bd9d3d8609c0aad0602790f83bad9c14
suganeswaran <nsuganeswaran@gmail.com>
2023-10-27 23:43:42 +0530

    Add room model

---
+++ Models/Room.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { RoomTypes } = require("../Constants/DbTypes");
+
+const roomSchema = new mongoose.Schema(
+  {
+    number: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    hotelId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hotel",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: RoomTypes,
+      required: true,
+    },
+    availability: {
+      type: Boolean,
+      required: true,
+    },
+    pricePerDay: {
+      type: Number,
+      required: true,
+    },
+    cancellationPolicy: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Room = mongoose.model("Room", roomSchema);
+
+module.exports = Room;
