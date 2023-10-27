41cf5e63371939a287f7fc31da039f2b9621cee1
suganeswaran <nsuganeswaran@gmail.com>
2023-10-27 23:42:40 +0530

    Add types for model

---
+++ Constants/DbTypes.js
@@ -0,0 +1,4 @@
+export const UserTypes = ["Standard", "Staff", "Admin", "SuperAdmin"];
+export const HotelTypes = ["Resort", "Hotel", "Motel", "Lodge"];
+export const RoomTypes = ["Standard", "Double", "Twin"];
+export const BookingStatusTypes = ["Booked", "Pending"];
