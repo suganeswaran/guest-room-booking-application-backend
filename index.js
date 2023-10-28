d3a9a5c905229c065892831ca4f0c0717f96f007
suganeswaran <nsuganeswaran@gmail.com>
2023-10-28 12:19:47 +0530

    Add cookie-parser

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 require("dotenv").config();
 const router = require("./Routes");
+const cookieParser = require("cookie-parser");
 require("./db.conection");
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use("/", router);
